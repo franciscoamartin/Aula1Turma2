b6615b57a87fc8901e924b5a7895898e74f89803
Sss41 <silvana.souza63@gmail.com>
2019-09-10 17:22:58 -0300

    Solicitar livro na biblioteca

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Informe o livro a ser consultado: ");
+            var consulta = Console.ReadLine();
+            if (consulta == "123456") {
+                Console.WriteLine("Não disponível");
+                Console.ReadKey();
+
+                return;
+            } else {
+                Console.WriteLine("Deseja alocar o livro (1)Sim (2)Não");
+                var resposta = Console.ReadLine();
+                if (resposta == "1") {
+                    Console.WriteLine("Livro alocado");
+                    Console.ReadKey();
+                    //finaliza o metodo.
+                    return;
+
+
+                }
+            }
+        }
+
+    }
+}
