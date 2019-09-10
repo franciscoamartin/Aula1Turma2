50325c6665310579a8e039120bceb6073a4e39a0
Sss41 <silvana.souza63@gmail.com>
2019-09-10 14:56:52 -0300

    biblioteca

--- Solution2/ConsoleApp2/Program.cs
+++ Solution2/ConsoleApp2/Program.cs
@@ -1,14 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp2 {
     class Program {
         static void Main(string[] args) {
 
             Console.WriteLine(" ");
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+
+            if (a == "v" && b == a) {
+                Console.WriteLine("Verdadeiro");
+            } else if
+                (a == "f" && b == a) {
+                Console.WriteLine("Falso");
+            } else if (a == b || a != b) {
+                Console.WriteLine(" Verdadeiro");
+            } else { 
+                Console.WriteLine(" Falso");
+            }
+
         }
     }
 }
+
