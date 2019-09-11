15513dc6cc4432185c9d4c1c148e0ce19abfff3b
Sss41 <silvana.souza63@gmail.com>
2019-09-11 09:03:36 -0300

    Atualizacao identacao

--- Biblioteca/Biblioteca/Program.cs
+++ Biblioteca/Biblioteca/Program.cs
@@ -16,13 +16,11 @@ namespace ConsoleApp1 {
 
             int biblioteca01 = 01;
             int biblioteca02 = 02;
-            string [] livros1 = new string[] { "01 - Matemática", " 02 - Portugûes", "03 - Inglês" };
+            string[] livros1 = new string[] { "01 - Matemática", " 02 - Portugûes", "03 - Inglês" };
 
             int biblioteca = int.Parse(Console.ReadLine());
             if (biblioteca == biblioteca01) {
                 Console.WriteLine("Biblioteca 01 selecionada! ");
-              //  livros1 = new string[] { "01 - Matemática", " 02 - Portugûes", "03 - Inglês" };
-
                 Console.WriteLine("Livros disponíveis:");
 
 
@@ -32,36 +30,27 @@ namespace ConsoleApp1 {
 
             } else if (biblioteca == biblioteca02) {
                 Console.WriteLine("Biblioteca 02 selecionada! ");
-
             } else if (biblioteca == biblioteca02) {
                 Console.WriteLine("Biblioteca 03 selecionada! ");
             } else {
                 Console.WriteLine("Biblioteca inválida! ");
             }
-
             Console.WriteLine("Escolha o livro pelo número: ");
 
             int escolherlivro = int.Parse(Console.ReadLine());
             for (int i = 0; i < livros1.Length; i++) {
-
             }
-            Console.WriteLine("Livro escolhido foi " + livros1[escolherlivro-1].ToString());
+            Console.WriteLine("Livro escolhido foi " + livros1[escolherlivro - 1].ToString());
             Console.WriteLine("Você quer locar esse livro? (1)SIM (2)NÃO");
             int locarLivro = int.Parse(Console.ReadLine());
 
-            if(locarLivro == 1 || locarLivro == 2) {
+            if (locarLivro == 1 || locarLivro == 2) {
                 Console.WriteLine("Livro Locado!");
-            }
-            else { 
+            } else {
                 Console.WriteLine("Digite o número correto!");
-                
+
             }
         }
-
-
-
-
-
 
     }
 }
