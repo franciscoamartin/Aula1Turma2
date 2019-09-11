0a724b8255544747b38059659d2834446e7a0249
franciscoamartin <franciscoaugustomartin@gmail.com>
2019-09-10 22:43:37 -0300

    Sistema Biblioteca 2.0

--- SistemaBiblioteca/SistemaBiblioteca/Program.cs
+++ SistemaBiblioteca/SistemaBiblioteca/Program.cs
@@ -16,25 +16,21 @@ namespace ConsoleApp1 {
 
             int biblioteca01 = 01;
             int biblioteca02 = 02;
-            int biblioteca03 = 03;
+
             string[] livros1 = new string[] { "01 - Matemática", " 02 - Portugûes", "03 - Inglês" };
 
             int biblioteca = int.Parse(Console.ReadLine());
             if (biblioteca == biblioteca01) {
                 Console.WriteLine("Biblioteca 01 selecionada! ");
-                //  livros1 = new string[] { "01 - Matemática", " 02 - Portugûes", "03 - Inglês" };
 
                 Console.WriteLine("Livros disponíveis:");
-
 
                 for (int i = 0; i < livros1.Length; i++) {
                     Console.WriteLine(livros1[i].ToString());
                 }
-
             }
             else if (biblioteca == biblioteca02) {
                 Console.WriteLine("Biblioteca 02 selecionada! ");
-
             }
             else if (biblioteca == biblioteca02) {
                 Console.WriteLine("Biblioteca 03 selecionada! ");
@@ -55,7 +51,8 @@ namespace ConsoleApp1 {
 
             if (locarLivro == 1) {
                 Console.WriteLine("Livro Locado!");
-            }else if(locarLivro == 2 ){
+            }
+            else if (locarLivro == 2) {
                 Console.WriteLine("Você não locou esse livro!");
             }
             else {
@@ -63,11 +60,6 @@ namespace ConsoleApp1 {
 
             }
         }
-
-
-
-
-
 
     }
 }
