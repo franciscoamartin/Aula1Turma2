a732c4d072fc06d659171b63a30f51ed118ed661
Francisco Martin <franciscoaugustomartin@gmail.com>
2019-09-12 17:11:43 -0300

    Aula 12/09

---
+++ LacosDeRepeticao/UtilizandoWhile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilizandoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var count = 0;
+            while (count <= 1000) {
+                Console.WriteLine(count++);
+                
+            }
+
+            Console.WriteLine("Fim do loop");
+            Console.ReadKey();
+        }
+    }
+}
