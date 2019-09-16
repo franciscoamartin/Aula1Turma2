1b0c4c65c5a6c010c1be8fb36144f0e1e2458e01
Francisco Martin <franciscoaugustomartin@gmail.com>
2019-09-16 17:22:19 -0300

    Exercicios 16/09 Adicionar Itens a lista

---
+++ AprendendoOList/AprendendoOList/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Threading;
+
+namespace AprendendoOList
+{
+    class Program
+    {
+        static List<string> list = new List<string>() {
+
+                "francisco",
+                "Irineu",
+                "Serilope"
+             };
+
+        static void Main(string[] args)
+        {
+            AdicionarItensALista();
+            ListaInformacoes();
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// metodo que adiciona um item a lista
+        /// </summary>
+        private static void AdicionarItensALista()
+        {
+            Console.Clear();
+            Console.WriteLine("Informe um nome");
+            var nome = Console.ReadLine();
+            list.Add(nome);
+            Console.WriteLine(nome + " adicionado na lista");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Deseja informar mais um nome? (s)SIM (n)NÃO ");
+            if (Console.ReadKey().KeyChar.ToString().ToLower() == "s")
+            {
+                AdicionarItensALista();
+
+            }
+        }
+        /// <summary>
+        /// metodo que mostra a lista
+        /// </summary>
+        private static void ListaInformacoes()
+        {
+            Console.WriteLine("Nomes Adicionados na Lista");
+
+            foreach (var minhaLista in list)
+            {
+                Console.WriteLine(minhaLista);
+            }
+        }
+
+    }
+}
---
+++ MaiorIdade/MaiorIdade/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MaiorIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsultaNomes();
+        }
+
+        private static void ConsultaNomes()
+        {
+            var conteudo = "nome:Felipe,idade:27;nome:Giomar,idade:17;nome:Edson,idade:19;nome:Ericledson,idade:75;nome:Junior,idade:45";
+
+            ListarInformacoes(conteudo);
+
+            Console.ReadKey();
+        }
+
+        private static void ListarInformacoes(string conteudo)
+        {
+            var listaDeInformacoes = conteudo.Split(';');
+
+            foreach (var item in listaDeInformacoes)
+            {
+                var separandoInformacoes = item.Split(',');
+                var nomePessoa = separandoInformacoes[0].Split(':')[1];
+                var idade = separandoInformacoes[1].Split(':')[1];
+                if (int.Parse(idade) >= 18)
+
+                    Console.WriteLine($"{nomePessoa} com {idade} anos de idade é uma pessoa maior de (+18). "
+                        );
+
+            }
+        }
+
+    }
+}
