9a88699c7270ce1d972088f3c89b6a106c2553a3
Francisco Martin <franciscoaugustomartin@gmail.com>
2019-10-22 17:20:26 -0300

    Aplicacoes cep api

---
+++ WebApplication3/WebApplicationCorreios/Scripts/Utils/scripts.js
@@ -0,0 +1,15 @@
+﻿$('#ceporigem').mask("00000-000");
+$('#cepdestino').mask("00000-000");
+
+$('#limpar').click(function () {
+    ClearScreen();
+});
+
+
+
+function ClearScreen() {
+    var myInputs = $('input[type="text"]');
+    myInputs.val("");
+}
+
+
