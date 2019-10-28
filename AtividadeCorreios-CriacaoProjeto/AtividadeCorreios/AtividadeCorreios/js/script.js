98cc45ffb5ed3c87b9b3c4c19b04e35ac7b2de61
Francisco Martin <franciscoaugustomartin@gmail.com>
2019-10-28 17:17:23 -0300

    att API web Get e Post

---
+++ AtividadeCorreios-CriacaoProjeto/AtividadeCorreios/AtividadeCorreios/js/script.js
@@ -0,0 +1,21 @@
+﻿
+$(document).ready(function () {
+    $('input[name="bntconsultar"]').click(function () {
+       //obter as informações dos meus campos
+        //Obtemos as informações do nosso formulario web completo
+        var informacoes = $('form[name="formenviar"]').serializeArray();
+        //Obtemos o atributo que indica a pagina que vamos enviar o post
+        var obterAtributo = $('form[name="formenviar"]').attr('send-post');
+
+        //Enviamos o post para nosso servidor web
+        $.post("http://usysweb.com.br/api/" + obterAtributo + ".php?giomar=true", informacoes, function (data) {
+            //aqui como recebemos uma string com formato de JSON
+            //temos que passar ela para o JSON parecido com o int.Parse()
+            data = JSON.parse(data);
+            //Aqui carregamos as informações automaticamente
+            $.each(data.cServico, function (key, value) {
+                $('p[name="{key}"]'.replace("{key}", key)).text(value);
+            });
+        });
+    });
+});
---
+++ AtividadeCorreios-CriacaoProjeto/WebApiImc/WebApiImc/Models/ObjectImc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiImc.Models
+{
+    public class ObjectImc
+    {
+        public double Peso { get; set; } 
+
+        public double Altura { get; set; } 
+
+        public string Nome { get; set; }
+
+        public double Imcvalue { get; set; }
+
+    }
+}
