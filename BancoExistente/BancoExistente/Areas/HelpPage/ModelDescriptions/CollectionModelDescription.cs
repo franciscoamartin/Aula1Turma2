0c0e16855c913443fd5d929c20585a06b9a90ab1
Francisco Martin <franciscoaugustomartin@gmail.com>
2019-10-29 17:15:11 -0300

    Banco Postman Inner join

---
+++ BancoExistente/BancoExistente/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace BancoExistente.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ BancoExistente/BancoExistente/Controllers/CarroesController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BancoExistente.Models;
+
+namespace BancoExistente.Controllers
+{
+    public class CarroesController : ApiController
+    {
+        private DataBaseContext db = new DataBaseContext();
+
+        [HttpGet]
+        [Route("Api/Carroes/CustomQuery")]
+        public object CustomCarrosQuery()
+        {
+            var listaDeCarros = db.Carros.ToList();
+
+            var retornoCarros = from carros in listaDeCarros
+                                select new
+                                {
+                                    NomeCarro = carros.Modelo,
+                                    CarrosId = carros.Id
+                                };
+
+            return retornoCarros;
+        }
+
+
+        [HttpGet]
+        [Route("Api/Carroes/CarrosComMarcas")]
+        public object CustomCarrosOnMarcas()
+        {
+            var listaDeCarros = db.Carros.ToList();
+            var listMarcas = db.Marcas.ToList();
+
+            var conteudoRetorno = from mar in listMarcas
+                                  join car in listaDeCarros
+                                  on mar.Id equals car.Marca
+                                   select new {
+                                       CarrosId = car.Id,
+                                       CarroNome = car.Modelo,
+                                       MarcaId = mar.Id,
+                                       MarcaNome = mar.Nome
+                                   };
+            return conteudoRetorno;
+        }
+
+        [HttpGet]
+        [Route("Api/Usuario/UsuarioComCarros")]
+        public object CustomUsuarioOnCarros(int idCarros)
+        {
+            var listaDeCarros = db.Carros.Where(p => p.Id == idCarros).ToList();
+            var listUsuarios = db.Usuarios.ToList();
+
+            var conteudoRetorno = from usu in listUsuarios
+                                  join car in listaDeCarros
+                                  on usu.Id equals car.UsuInc
+                                  select new
+                                  {
+                                      CarrosId = car.Id,
+                                      CarroNome = car.Modelo,
+                                      Usuario = usu.Id,
+                                      UsuarioNome = usu.Usuario1
+                                  };
+            return conteudoRetorno;
+        }
+
+
+
+
+        // GET: api/Carroes
+        public IQueryable<Carro> GetCarros()
+        {
+            return db.Carros;
+        }
+
+        // GET: api/Carroes/5
+        [ResponseType(typeof(Carro))]
+        public IHttpActionResult GetCarro(int id)
+        {
+            Carro carro = db.Carros.Find(id);
+            if (carro == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(carro);
+        }
+
+        // PUT: api/Carroes/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCarro(int id, Carro carro)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != carro.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(carro).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CarroExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Carroes
+        [ResponseType(typeof(Carro))]
+        public IHttpActionResult PostCarro(Carro carro)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Carros.Add(carro);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = carro.Id }, carro);
+        }
+
+        // DELETE: api/Carroes/5
+        [ResponseType(typeof(Carro))]
+        public IHttpActionResult DeleteCarro(int id)
+        {
+            Carro carro = db.Carros.Find(id);
+            if (carro == null)
+            {
+                return NotFound();
+            }
+
+            db.Carros.Remove(carro);
+            db.SaveChanges();
+
+            return Ok(carro);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CarroExists(int id)
+        {
+            return db.Carros.Count(e => e.Id == id) > 0;
+        }
+    }
+}
