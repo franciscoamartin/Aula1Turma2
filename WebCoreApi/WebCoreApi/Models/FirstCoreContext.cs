d8b0c11fa8593fe5a7a324ae55c0cc733bca9af2
Francisco Martin <franciscoaugustomartin@gmail.com>
2019-11-01 17:28:31 -0300

    commit sexta - web api core 3.0

---
+++ WebCoreApi/WebCoreApi/Models/FirstCoreContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebCoreApi.Models
+{
+    public class FirstCoreContext : DbContext
+    {
+        public FirstCoreContext(DbContextOptions<FirstCoreContext> options )
+            : base (options)
+        {
+
+        }
+
+        public DbSet<Carro> Carros { get; set; }
+    }
+}
