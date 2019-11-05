bba5815fb80ca453f16194c742c6a29d0d48e107
Francisco Martin <franciscoaugustomartin@gmail.com>
2019-11-05 16:57:11 -0300

    Att WebApi - login - Core

---
+++ LoginUsuarios/LoginUsuarios/Models/UsuarioContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace LoginUsuarios.Models
+{
+    public partial class UsuarioContext : DbContext
+    {
+        public UsuarioContext()
+        {
+        }
+
+        public UsuarioContext(DbContextOptions<UsuarioContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Usuarios> Usuarios { get; set; }
+
+   
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuarios>(entity =>
+            {
+                entity.Property(e => e.Email)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Login)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Senha)
+                    .HasColumnName("senha")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ LoginUsuarios/LoginUsuarios/View/js/script.js
@@ -0,0 +1,53 @@
+﻿
+var Cadastro =
+{
+    Nome: "teste",
+    Senha: "senha",
+    Login: "login",
+    Email: "email"
+}
+
+var ArrayCadastro = [Cadastro];
+
+$(document).ready(
+    function () {
+        $('#show').click(function () {
+
+            //alert(JSON.stringify(ArrayDogs));
+
+            var stringTemplate = "Registrado com sucesso:\n\n";
+
+            for (var i = 0; i < ArrayCadastro.length; i++) {
+                stringTemplate += ArrayCadastro[i].Name + "\r\n";
+            }
+
+            alert(stringTemplate);
+            ClearScreen();
+        });
+
+        $('#save').click(function () {
+            var NovoCadastro =
+            {
+                Email: $('#email').val(),
+                Senha: $('#senha').val(),
+                Login: $('#login').val(),
+                Nome: $('#nome').val()
+
+
+            }
+
+            ArrayCadastro.push(NovoCadastro);
+            var stringResult = "Cadastro salvo!\r\nEmail: " + NovoCadastro.email + "\r\nLogin: " + NovoCadastro.Login + " \r\nnome " + + NovoCadastro.Nome + "\r\nSenha: " + NovoCadastro.Senha;
+            alert(stringResult);
+            ClearScreen();
+
+        });
+
+    });
+function ClearScreen() {
+    var myInputs = $('input[type="text"]');
+    myInputs.val("");
+}
+
+
+
